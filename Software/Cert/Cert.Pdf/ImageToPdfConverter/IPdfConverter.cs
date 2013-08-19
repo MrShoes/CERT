baf0e99b6c8312497495f77061a1040007a9a182
Ian Hughes <ianhughes81@googlemail.com>
2013-08-19 22:02:48 +0100

    Added iTextSharp reference
    Also started IPdfConverter interface.

---
+++ Software/Cert/Cert.Pdf/ImageToPdfConverter/IPdfConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Cert.Pdf.ImageToPdfConverter
+{
+    public interface IPdfConverter
+    {
+        string FilePath { get; set; }
+
+        bool ConvertImageToPdf(Image image);
+    }
+}
